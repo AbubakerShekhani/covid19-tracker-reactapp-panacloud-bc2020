1a011e03dd424f6dcd91fbb44b3b455e18b90077
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-13 14:53:53 +0500

    Add logic and data display

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api';
+import { Bar, Line } from 'react-chartjs-2'
+import styles from './Chart.module.css'
 
-const Chart = () => {
+const Chart = (props) => {
+
+  const country     = props.country
+  const summaryData = props.data
+
+  const [dailyStatisticsData, setdailyStatisticsData] = useState({})
+
+  useEffect(() => {
+    const fetchDailyDataFromAPI = async () =>
+    {
+      const initialDailyData = await fetchDailyData()
+      setdailyStatisticsData(initialDailyData)
+    }
+
+    fetchDailyDataFromAPI()
+  }, [])
+
+  console.log(`${country} from props from state`);
+  console.log("Data for charts data")
+  console.log(dailyStatisticsData)
+
+
+  const lineChart = (
+    dailyStatisticsData[0] ? (
+      <Line
+        data={{
+          labels: dailyStatisticsData.map(( { date }) => date ),
+          datasets:[{
+            data: dailyStatisticsData.map((data) => data.confirmed),
+            label: 'Infected Cases',
+            borderColor: '#333fff',
+            fill:true
+          }, {
+            data: dailyStatisticsData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0,0 0.5)',
+            fill: true,
+          },
+        ],
+        }}
+      />
+    ) :null
+    );
+
   return (
-    <h1>Chart</h1>
+    <div className={styles.container}>
+      { lineChart }
+    </div>
   )
 }
 
