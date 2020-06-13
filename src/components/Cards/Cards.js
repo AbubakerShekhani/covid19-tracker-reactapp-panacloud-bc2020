fc17a9732c66a24186772c183af0d93c81f1d270
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-13 14:53:26 +0500

    Add data fetch logic

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import CountUp from 'react-countup';
 
 const Cards = ({summaryData}) => {
 
-  const { confirmed } = summaryData;
+  const { confirmed, recovered, deaths, lastUpdate } = summaryData;
 
   if (!confirmed) {
     return 'Loading...';
+  } else {
+    console.log(summaryData)
   }
 
   return (
     <div>
     <h1>Cards</h1>
-    { console.log( confirmed.value ) }
-    <p>{confirmed.value}</p>
+    <p>Total Confirmed: <CountUp start={0} end={confirmed.value} duration={1.5} separator="," /></p>
+    <p>Total Recovered: <CountUp start={0} end={recovered.value} duration={1.5} separator="," /></p>
+    <p>Total Deaths: <CountUp start={0} end={deaths.value} duration={1.5} separator="," /></p>
+    <p>Last Updated: {new Date(lastUpdate).toDateString()}</p>
     </div>
   )
 }
