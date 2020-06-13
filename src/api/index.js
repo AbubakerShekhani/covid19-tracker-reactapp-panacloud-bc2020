437bb69d3e71e76c539c4dd9162ee37fd3736a69
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-13 14:54:50 +0500

    Add fetchDataByCountry and dailydata

--- src/api/index.js
+++ src/api/index.js
@@ -2,17 +2,32 @@ import axios from 'axios'
 
 const baseURL = 'https://covid19.mathdro.id/api'
 
+const baseURL2 = 'https://api.covid19api.com/'
+
 export const fetchSummaryData = async () => {
   try {
     const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(baseURL)
 
     const summaryData = { confirmed,recovered, deaths, lastUpdate }
 
-
-
     return summaryData
   } catch (error) {
+    return error;
+  }
+}
 
+export const fetchDataByCountry = async (country) => {
+
+  let countryURL = `${baseURL}/countries/${country}`;
+
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(countryURL)
+    const summaryData = { confirmed,recovered, deaths, lastUpdate }
+
+    return summaryData
+
+  } catch(err) {
+      return err;
   }
 }
 
@@ -25,3 +40,17 @@ export const fetchCountries = async() => {
     return err;
   }
 }
+
+
+export const fetchDailyData = async () => {
+  try {
+
+    const { data } = await axios.get(`${baseURL}/daily`);
+
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+
+  } catch (error) {
+    return error;
+  }
+};
